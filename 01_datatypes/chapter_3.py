57045579f0dc2e658aa45faa09b4d053c4e4df22
Kartik Konje <kartikkonje.01@gmail.com>
2026-01-03 03:16:16 +0530

    Boolean, logical operators & operations

---
+++ 01_datatypes/chapter_3.py
@@ -0,0 +1,22 @@
+
+# Boolean Data Type
+
+is_raining = True
+is_sunny = False
+print("Is it raining?", is_raining)
+print("Is it sunny?", is_sunny)
+
+boolean_sum = is_raining + is_sunny  # upcasting
+print("Sum of boolean values (True=1, False=0):", boolean_sum)
+
+# Logical Operations
+
+and_result = is_raining and is_sunny
+print("Logical AND (is_raining and is_sunny):", and_result)
+
+or_result = is_raining or is_sunny
+print("Logical OR (is_raining or is_sunny):", or_result)
+
+not_raining = not is_raining
+print("Logical NOT (not is_raining):", not_raining)
+
